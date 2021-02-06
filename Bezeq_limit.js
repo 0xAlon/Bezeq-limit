eca435fef279a66bef232743cc6e9d467bfef308
Alon <alon.ttp@gmail.com>
2021-02-06 18:32:22 +0200

    update functionality

--- Bezeq_limit.js
+++ Bezeq_limit.js
@@ -1,29 +1,70 @@
+// Include modules
 const puppeteer = require('puppeteer');
 const fs = require('fs');
+const cronJob = require('cron').CronJob;
+var nodemailer = require('nodemailer');
 
-var password = NaN
-var username = NaN
-var value = NaN
+// Global variables
+let username = NaN
+let site_password = NaN
+let email = NaN
+let email_password = NaN
+let value = NaN
 
-
+/**
+     * Get configuration details from json file.
+     * @function config
+*/
 function config(){
-	let myPromise = new Promise(
+	let configPromise = new Promise(
 		(resolve, reject) => {
 			fs.readFile('./config.json', (err, data) => {
-				if (err) throw err;
-				let config = JSON.parse(data);
-				password = config[0]['password']
-				username = config[0]['username']
-				resolve('configure successful')
+				if (err){
+					reject(err);
+				}
+				else{
+					let config = JSON.parse(data);
+					username = config[0]['username']
+					site_password = config[0]['password']
+					email = config[1]['username']
+					email_password = config[1]['password']
+					resolve('configure successful')
+				}
 			});
 		}
 	);
-	return myPromise;
+	return configPromise;
 }
 
+function notification(){
+	// Allow less secure apps: ON
+
+	var transporter = nodemailer.createTransport({
+		service: 'gmail',
+		auth: {
+		user: email,
+		pass: email_password
+		}
+	});
+	
+	var mailOptions = {
+		from: 'noreply@bezeq-limits.com',
+		to: 'alon.ttp@gmail.com',
+		subject: 'Bezeq limits',
+		text: value
+	};
+	
+	transporter.sendMail(mailOptions, function(error, info){
+		if (error) {
+		console.log(error);
+		} else {
+		console.log('Email sent: ' + info.response);
+		}
+	});
+}
 
 function getData() {
-	let myPromise = new Promise(
+	let getDataPromise = new Promise(
 		(resolve, reject) => {
 			(async () => {
 				const browser = await puppeteer.launch({
@@ -41,7 +82,6 @@ function getData() {
 				const page = await browser.newPage();
 				const query = "https://bmy.bezeq.co.il/actions/login?appid=WORTHINESS&WT.isp=menu"
 				await page.goto(query, {waitUntil : 'domcontentloaded' }).catch(e => void 0)
-				
 				const selector = '#loginBySms';
 				await page.waitForSelector(selector);
 				await page.click("#tabpanel > div > div.floatDiv.logintype.loginIcon.notactive");
@@ -50,11 +90,11 @@ function getData() {
 				await page.type(selector1, username);
 				const selector2 = '#loginByUsername > form > fieldset > div:nth-child(3) > div:nth-child(2) > div > input';
 				await page.waitForSelector(selector2);
-				await page.type(selector2, password);
+				await page.type(selector2, site_password);
 				const selector3 = "#loginByUsername > form > fieldset > div.floatDivLeft > div:nth-child(1) > button"
 				await page.waitForSelector(selector3);
 				await page.click(selector3);
-				console.log('\x1b[33m%s\x1b[0m','Login successful!');
+				console.log('Login successful!');
 				const selector4 = '#MainCategory_31 > div > div > div.widgetsRows.clearfix > div:nth-child(2) > div:nth-child(3)';
 				await page.waitForSelector(selector4);
 				await page.click("#Widget_186 > div > div");
@@ -62,25 +102,37 @@ function getData() {
 				await page.waitForSelector("#extension_188 > div > div > div.records.ng-scope");
 				const element = await page.waitForSelector(selector5);
 				value = await page.evaluate(element => element.textContent, element);
-				//console.log(value);
 				resolve(value)
 				await browser.close();
 			})();
 		}
 	);
-	return myPromise;
+	return getDataPromise;
 }
 
-config().then( (result) => {
-	console.log('\x1b[33m%s\x1b[0m',result)
-	getData().then( (result) => {
-
-		if(result > 20){
-			console.log(result);
-		}
-		else{
-			emailNotification()
-			console.log(result);
-		}
-	});
+const job = new cronJob('*/300 * * * * *', () => {
+	config().then( 
+		(result) => { console.log(result)
+			getData().then( 
+				(result) => {
+					if(result > 20){
+						console.log(result);
+					}
+					else{
+						console.log(result);
+						if(result == 0){
+							job.start();
+						}
+						else{
+							notification()
+						}
+					}
+				},
+				(error) => { console.log("Bad error:" + error) }
+			);
+		},
+		(error) => { console.log("Bad error:" + error) }
+	);
 });
+
+job.start();
